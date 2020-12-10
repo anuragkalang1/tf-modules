3a7b4f07c6020daf7d86970eb5530832c8c26bd2
anuragkalang1 <48652172+anuragkalang1@users.noreply.github.com>
2020-12-10 13:47:46 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,75 @@
+provider "aws" {
+   region = "${var.aws_region}"
+   version = "<= 2.8.0"
+ }
+
+terraform {
+  required_version = "= 0.13"
+  backend "s3" {}
+}
+
+data "aws_security_group" "ports" {
+   name = "test"
+   vpc_id = "${local.vpc_id}"
+}
+
+resource "aws_security_group" "test1" {
+   vpc_id = "${local.vpc_id}"
+   count = "${var.namespace_enabled}"
+   
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    security_groups = ["${data.aws_security_group.ports.id}"]
+  }
+  
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_launch_configuration" "hello_world_deploy" {
+  name_prefix                   = "Helloworld-"
+  image_id                      = "${var.ami}"
+  key_name                      = "${var.keypair}"
+  associate_public_ip_address   = false    
+  instance_type                 = "${var.instance_type}"  
+  security_groups 			        = ["${aws_security_group.test1.id}"] 
+  root_block_device {        
+    volume_type 				        = "${var.volume_type}"
+    volume_size 				        = "${var.volume_size}"
+    delete_on_termination 			= "${var.del_on_term}"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+  user_data = "${base64encode(data.template_file.helloworld_data)}"
+}
+
+resource "aws_autoscaling_group" "hello_world_asg" {
+  name                 = "helloworld-asg"
+  launch_configuration = "aws_launch_configuration.hello_world_deploy"
+  health_check_type    = "EC2"
+  desired_capacity     = 1
+  max_size             = 1
+  min_size             = 1 
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "template_file" "helloworld_data" {
+  template = <<EOF
+                              
+    <shell>
+    # Add the code to be deployed, I am deploying using ansible
+    #Note: This infrastructure should be created before we run the ansible playbook
+    <shell>
+    <persist>true</persist>
+EOF
+}
